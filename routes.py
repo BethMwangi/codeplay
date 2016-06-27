62d9371dd1a577ecb449e67383127601d4770979
Elsis-Sitati <sitatielsis@gmail.com>
2016-06-27 22:08:49 +0300

    made routes for register

--- routes.py
+++ routes.py
@@ -11,7 +11,6 @@ def index():
     # when there is no username in the session
     return render_template('index.html')
 
-
 # creating route for login
 @app.route('/login', methods = ['POST'])
 def login():
@@ -28,4 +27,28 @@ def login():
             return redirect(url_for ('index'))
     # if login_user doesnt exist or password is wrong
     return 'Invalid username/password combination'
+
+# creating route for register
+@app.route('/register', methods = ['POST', 'GET'])
+def register():
+    # when the request made is a post method
+    if request.method == 'POST':
+        # checking if username already exist
+        users = mongo.db.users
+        # searching the data
+        existing_user = users.find_one({'name' : request.form['username']})
+
+        # if it finds nothing in the database go ahead and register
+        if existing_user is None:
+            # first hash the password before storing in database
+            hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
+            # inserting the user information into the users collection
+            users.insert({'name': request.form['username'], 'password': hashpass})
+            # creating session
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+        # the return option if username already exists
+        return 'That username already exists'
+    return render_template('register.html')
+
 
