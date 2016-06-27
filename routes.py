97bbaba56fcb5adbfea74d551e44d683e6cf6f91
Elsis-Sitati <sitatielsis@gmail.com>
2016-06-27 22:07:18 +0300

    made routes for login

--- routes.py
+++ routes.py
@@ -11,3 +11,21 @@ def index():
     # when there is no username in the session
     return render_template('index.html')
 
+
+# creating route for login
+@app.route('/login', methods = ['POST'])
+def login():
+    # collection for the database
+    users = mongo.db.users
+    # find the login name in the database
+    login_user = users.find_one({'name' : request.form['username']})
+    # condition for when the login_user exists
+    if login_user:
+        # comparing passwords
+        if bcrypt.hashpw(request.form['password'].encode('utf-8'),login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
+            # add user to the session
+            session['username'] = request.form['username']
+            return redirect(url_for ('index'))
+    # if login_user doesnt exist or password is wrong
+    return 'Invalid username/password combination'
+
