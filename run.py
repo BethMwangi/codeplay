54fa85629ab98d5849e971ec136048deffd158c4
yvonnendutaw <yvonita7@gmail.com>
2016-06-30 18:03:33 +0300

    added config

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 # This script runs the allication using a development server
 
 # creating flask object
@@ -12,5 +12,4 @@ app = Flask(__name__)
 if __name__ == '__main__':
     app.secret_key = 'mysecret'
     app.run(debug=True)
-=======
->>>>>>> 10dad6bfd1fffecc6804b70b539670453e9be049
+
