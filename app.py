71d5bda6433eed2de7e1bf228200c3282232e177
yvonnendutaw <yvonita7@gmail.com>
2016-06-30 16:23:54 +0300

    merge

--- app.py
+++ app.py
@@ -3,23 +3,10 @@
 # creating flask object
 
 from flask import Flask
-from flask_bcrypt import Bcrypt
-from flask_pymongo import PyMongo
+
 
 app = Flask(__name__)
-# setting up Bcrypt
-bcrypt = Bcrypt(app)
-# adding Pymongo
-mongo = PyMongo(app)
 
-# making the WSGI interface accessible at top level
-wsgi_app = app.wsgi_app
-
-# import all of our routes from routes routes.py
-
-from routes import *
-
-# Launching our server
 
 if __name__ == '__main__':
     app.secret_key = 'mysecret'
