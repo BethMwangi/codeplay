a16f324402833f1d7ccf23b9e8ad04bbafa8d847
misoi <cytnhia.chepkemoi@gmail.com>
2016-07-03 14:08:34 +0300

    add views route

--- app/views.py
+++ app/views.py
@@ -1,25 +1,52 @@
-from codeplay import app
+from app import app
 from flask import render_template, request, flash, session, url_for, redirect
-from forms import SignupForm
-from models import db, User
+from forms import SignupForm, PostForm
+from models import db, Posts
+from flask.ext.login import login_required, current_user
 
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    form = SignupForm()
+
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def hello_world():
+	form = PostForm()
 	if request.method == 'POST':
-		if form.validate() == False:
-            return render_template('signup.html', form=form)
-		else:
-			newuser = User(form.username.data, form.email.data, form.password.data)
-			db.session.add(newuser)
+	# if current_user.can(Permission.WRITE_ARTICLES) and \
+		if form.validate() == True:
+			post = Posts(post=form.post.data, author=current_user._get_current_object())
+			db.session.add(post)
 			db.session.commit()
+			return redirect(url_for('/'))
+		else:
+			flash('an error occured')
+		
+		posts = Posts.query.order_by(Posts.timestamp.desc()).all()
 
-			session['email'] = newuser.email
-			return redirect(url_for('home'))
+	return render_template('home.html', form=form)
+		
+# @app.route('/signup', methods=['GET', 'POST'])
+# def signup():
+#     form = SignupForm()
+# 	if request.method == 'POST':
+# 		if form.validate() == False:
+#             return render_template('signup.html', form=form)
+# 		else:
+# 			newuser = User(form.username.data, form.email.data, form.password.data)
+# 			db.session.add(newuser)
+# 			db.session.commit()
 
-	elif request.method == 'GET':
-		return render_template('signup.html', form=form)
+# 			session['email'] = newuser.email
+# 			return redirect(url_for('home'))
 
-@app.route('/signup', methods=['GET', 'POST'])
-def home('/index'):
-	return render_template('index.html')
+# 	elif request.method == 'GET':
+# 		return render_template('signup.html', form=form)
+
+# @app.route('/signup', methods=['GET', 'POST'])
+# def home('/index'):
+# 	return render_template('index.html')
+
+
+# ==================Route for home where users can post=================
+
+
